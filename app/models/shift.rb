2ce9f2641e831962b78ee4a1afe28653bc2478ae
Ashley Knight <ak@subcq.com>
2012-09-20 00:17:26 +1000

    Cleaned up View, Applied class to select, Working on hour calculation

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,4 +1,6 @@
 class Shift < ActiveRecord::Base
+  
+  
   attr_accessible :break_time, :day, :finish_time, :orders_to_place, :shift_type, :start_time, :store_id
   
   validates_presence_of :day, :finish_time, :start_time, :store_id
@@ -7,5 +9,13 @@ class Shift < ActiveRecord::Base
   DAYS = ['Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday']
   
   belongs_to :store
+  
+  def start_time_finish_time_friendly
+  "#{start_time .strftime('%l:%M %p')} - #{finish_time .strftime('%l:%M %p')}"
+  end
+  
+  def hours_worked
+  "((finish_time - start_time) / 1.hour).round"
+  end
   
 end
