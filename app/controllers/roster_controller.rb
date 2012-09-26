c0be8ea0d347336e8ed02eaf74083e689259d7aa
Darren Jeacocke <dazonic@me.com>
2012-09-26 23:12:35 +1000

    paginate weeks

--- app/controllers/roster_controller.rb
+++ app/controllers/roster_controller.rb
@@ -1,5 +1,7 @@
 class RosterController < ApplicationController
   def index
+    start_date = params[:start_date].blank? ? Store.next_week_start_day_date : Date.parse(params[:start_date])
+    @dates = start_date..(start_date + 6.days)
     @shifts = Shift.all
   end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -7,5 +7,14 @@ class Store < ActiveRecord::Base
   has_many :shifts
   has_many :employments
   has_many :employees, through: :employments
-  
+
+  WEEK_START_DAY = 3
+
+  def self.week_start_day_name
+    Date::DAYNAMES[WEEK_START_DAY]
+  end
+
+  def self.next_week_start_day_date
+    Chronic.parse("next #{week_start_day_name}").to_date
+  end
 end
